37c396d5b59efc3e1c1a06b1f32d2c398ca2b072
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-10 21:28:21 +0530

    updates in marketplace

--- frontend/src/pages/MarketPlace.jsx
+++ frontend/src/pages/MarketPlace.jsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import Cards from '../components/Cards';
+import { Link } from 'react-router-dom';
 
 
 const MarketPlace = () => {
@@ -30,21 +31,20 @@ const MarketPlace = () => {
     navigation={true}
     modules={[Autoplay, Pagination, Navigation]}
     
-    className='p-4 h-[60vh] max-w-fit my-5 rounded-2xl'
+    className='p-4 h-[60vh] w-[50%] my-5 rounded-2xl border-2 border-blue-500'
     id='swiper2'
     >
       <SwiperSlide>
         <div
           className="rec1 h-full relative  flex flex-col justify-center items-center text-white "
-          
-        >
-          <div className="absolute inset-0 bg-black opacity-60"></div>
+        > <Link to='/nftdetails'></Link>
+          {/* <div className="absolute inset-0 bg-black opacity-60"></div>
           <h2 className="text-4xl mb-4 z-10">first nft</h2>
           <p className="text-lg lg:text-xl mb-6 z-10 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est nam, qui maxime fuga totam.</p>
           
           <button className="bg-blue-500 hover:bg-blue-700 z-10 text-white font-bold py-2 px-4 rounded">
             Buy
-          </button></div>
+          </button>*/}</div> 
        
       </SwiperSlide>
 
@@ -52,8 +52,8 @@ const MarketPlace = () => {
         <div
           className="rec2 h-full relative  flex flex-col justify-center items-center text-white"
           
-        >
-          <div className="absolute inset-0 bg-black opacity-60"></div>
+        > <Link to='/nftdetails'></Link>
+          {/* <div className="absolute inset-0 bg-black opacity-60"></div>
           <h2 className="text-4xl mb-4 z-10">second nft</h2>
           <p className="text-xl mb-6 z-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem impedit optio accusantium!</p>
           <button
@@ -61,24 +61,15 @@ const MarketPlace = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white z-10 font-bold py-2 px-4 rounded"
           >
             Buy
-          </button>
+          </button> */}
         </div>
       </SwiperSlide>
 
       <SwiperSlide>
         <div
-          className="rec3 h-full relative  flex flex-col justify-center items-center text-white"
+          className="rec3 h-full relative  flex flex-col justify-center items-center text-white">
+            
           
-        >
-          <div className="absolute inset-0 bg-black opacity-60"></div>
-          <h2 className="text-4xl mb-4 z-10">third nft</h2>
-          <p className="text-xl mb-6 z-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem impedit optio accusantium!</p>
-          <button
-          
-            className="bg-blue-500 hover:bg-blue-700 text-white z-10 font-bold py-2 px-4 rounded"
-          >
-            Buy
-          </button>
         </div>
       </SwiperSlide>
      
