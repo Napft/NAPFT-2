7eb146ab5e1f68777c1f82d370d3231290098468
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2024-03-19 12:47:51 +0530

    Update deploy.js

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,29 +1,18 @@
-const { ethers } = require("hardhat");
-const { alchemyApiKey, mnemonic } = require("./secrets.json");
-const { NFTmarketplaceABI, NFTmarketplaceBytecode } = require("./constants");
+const hre = require("hardhat");
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mumbai.g.alchemy.com/v2/${alchemyApiKey}`);
-  const wallet = new ethers.Wallet(mnemonic, provider);
 
-  // Create the contract factory
-  const NFTmarketplaceFactory = new ethers.ContractFactory(
-    NFTmarketplaceABI,
-    NFTmarketplaceBytecode,
-    wallet
+  const NftContract = await hre.ethers.getContractFactory("NFTmarketplace");
+  console.log("deploying nft smart contract");
+  const NFT = await NftContract.deploy();
+  console.log("NFT deployed to:", NFT.address);
+
+
+  console.log(
+    `compile nft smart contract with address ${NFT.target}`
   );
 
-  // Deploy the contract
-  const NFTmarketplace = await NFTmarketplaceFactory.deploy();
-
-  await NFTmarketplace.deployed();
-
-  console.log("NFTmarketplace deployed to:", NFTmarketplace.address);
+  // console.log("abi of contract", NFT.abi);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
