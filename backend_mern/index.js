e685214702e248ed4168756e9d6c7113785e7a5d
Satyapriya <mishrasatyapriya9@gmail.com>
2024-05-18 10:59:20 +0530

    added router for user and NFT and to avoid cors policy write codes in index.js

--- backend_mern/index.js
+++ backend_mern/index.js
@@ -11,13 +11,32 @@ import UserRouter from "./src/routers/UserRouter/User.Router.js";
 const app = express();
 dotenv.config();
 app.use(cors());
+const allowedOrigins = ["http://localhost:5173", "http://localhost:4000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Check if the origin is allowed or if it's a request from a browser extension (e.g., React DevTools)
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.startsWith("chrome-extension://")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // If you are using cookies or sessions
+  })
+);
+
 app.use(express.json()); // Parses the body if its in JSON format.
 connectDB();
 app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/api/v1/nft", NftRouter);
-app.use("/api/v1/user",UserRouter);
+app.use("/api/v1/user", UserRouter);
 
 const PORT = process.env.PORT || 8800;
 const MODE = process.env.DEV_MODE;
