953c72aa40ea8ec5fcd338b263bcec9697c51646
Satyapriya <mishrasatyapriya9@gmail.com>
2024-06-27 18:05:26 +0530

    .env changes

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -10,7 +10,9 @@ function Cards() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await Axios.get('http://localhost:8800/api/v1/nft/All_NFTs');
+        const res = await Axios.get(
+          `${import.meta.env.VITE_HOST}/api/v1/nft/All_NFTs`
+        );
         if (res.status !== 200) {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
